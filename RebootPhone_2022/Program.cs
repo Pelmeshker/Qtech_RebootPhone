cb6ec8cb0ce36056644c09d35826691738a61afc
Pelmeshker <80691391+Pelmeshker@users.noreply.github.com>
2021-08-30 16:00:40 +0300

    Апдейт после полевых испытаний

--- RebootPhone_2022/Program.cs
+++ RebootPhone_2022/Program.cs
@@ -2,11 +2,18 @@
 using System.Net.Http.Headers;
 using System.Text;
 
-for (int i = 1;  i<255; i++)
+//В цикле перебирается вся подсеть и по очереди перезагружаются телефонные аппараты
+for (int i = 1; i < 255; i++)
 {
     Console.WriteLine("192.168.1." + i);
     var answer = RedirectRequest("http://192.168.1." + i);
+    //На некоторых объектах аппараты игнорировали первую команду перезагрузки, 
+    //поэтому на каждый адрес сделал по два запроса, решение не самое изящное, но работает безотказно
+    var answer2 = RedirectRequest("http://192.168.1." + i);
+    Console.WriteLine("Аппарат успешно перезагружен");
 }
+
+
 
 static async Task<HttpResponseMessage> RedirectRequest(string address)
 {
@@ -14,36 +21,40 @@ static async Task<HttpResponseMessage> RedirectRequest(string address)
 
     try
     {
+
         HttpResponseMessage response;
+
+        //Блок авторизации, генерирующий заголовок авторизации с парой логин\пароль
         client.Timeout = TimeSpan.FromSeconds(1);
         var clientMethod = "/goform/Reboot";
         var user = "login";
         var password = "password";
         var base64String = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{user}:{password}"));
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64String);
-        var requestUri = new Uri(address + clientMethod);
 
+        //Непосредственно POST запрос
+        var requestUri = new Uri(address + clientMethod);
         using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, address + clientMethod))
         {
             response = client.SendAsync(requestMessage).Result;
         }
 
+        //На этом этапе происходит редирект на страницу, которая отправляет команду перезагрузки,
+        //но заголовки авторизации при этом теряются, поэтому пришлось самому сделать необходимый GET запрос
         if (response.StatusCode == HttpStatusCode.Unauthorized)
         {
-            var finalRequestUri = response.RequestMessage.RequestUri; 
-            Console.WriteLine(finalRequestUri);
-            if (finalRequestUri != requestUri) 
+            var finalRequestUri = response.RequestMessage.RequestUri;
+            if (finalRequestUri != requestUri)
             {
-
-                Console.WriteLine(finalRequestUri.Host);
-                if (("http://" + finalRequestUri.Host) == address) 
+                if (("http://" + finalRequestUri.Host) == address)
                 {
                     return response = client.GetAsync(finalRequestUri).Result;
                 }
-            } else
+            }
+            else
             {
-                Console.WriteLine("Аппарат не отработал, повторный запрос");
-                return await RedirectRequest(address);
+                //Изначально была идея запускать здесь циклично метод до победного, но это оказалось лишним
+                return null;
             }
         }
 
@@ -56,6 +67,7 @@ static async Task<HttpResponseMessage> RedirectRequest(string address)
     {
         client.Dispose();
     }
+
 
     return null;
 
